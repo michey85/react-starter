435e4bc9faa55d9327fa0ed9f75340c325434d83
Mikhail Nepomniashchii <michey85@gmail.com>
2024-07-13 13:49:06 +0200

    fix linter settings

--- eslint.config.js
+++ eslint.config.js
@@ -46,7 +46,7 @@ export default tseslint.config(
       'react/function-component-definition': ['warn', { namedComponents: 'arrow-function' }],
       'react/self-closing-comp': ['error', { component: true, html: true }],
       'max-lines': ['warn', { max: 124 }],
-      'max-params': ['error', '3'],
+      'max-params': ['error', 3],
     },
   },
 );
